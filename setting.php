a003a5d825abeabd880d2ac2febe6d6c609c6448
Harryx77 <52307152+Harryx77@users.noreply.github.com>
2019-10-06 23:12:21 +0700

    Add files via upload

---
+++ setting.php
@@ -0,0 +1,21 @@
+<?php
+$res="\033[0m";$hitam="\033[0;30m";$abu2="\033[1;30m";$putih="\033[0;37m";$putih2="\033[1;37m";
+$red="\033[0;31m";$red2="\033[1;31m";$green="\033[0;32m";$green2="\033[1;32m";$yellow="\033[0;33m";
+$yellow2="\033[1;33m";$blue="\033[0;34m";$blue2="\033[1;34m";$purple="\033[0;35m";$purple2="\033[1;35m";
+$lblue="\033[0;36m";$lblue2="\033[1;36m";$des = "{$green2}>{$red2}X{$green2}<";$des1="{$yellow2}*";
+$database = "uNubwbVT";
+
+$popstellar = "{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}
+    .___               .___       ____   ____.___.___.___.
+  __| _/____  ____   __| _/____   \   \ /   /|   |   |   |
+ / __ |/ ___\/  _ \ / __ |/ __ \   \   Y   / |   |   |   |
+/ /_/ \  \__(  <_> ) /_/ \  ___/    \     /  |   |   |   |
+\____ |\___  >____/\____ |\___  >    \___/   |___|___|___|
+     \/    \/           \/    \/
+{$putih2}  Salam Damai Tuyuler Indonesia!!      {$red2}『 {$putih2}BOT POP STELLER{$red2} 』
+{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}
+{$lblue2}Created By {$abu2} :{$res} Mc. Queen
+{$lblue2}Group Tele{$abu2}  :{$res} t.me/jancokkau990";
+$pass_pop="2xrQMyPR";$upsandi_pop="Jancox";
+
+?>
