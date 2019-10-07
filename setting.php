1fef1e95e9b448892c6b0a9b6cf38319b7d0fecc
Harryx77 <52307152+Harryx77@users.noreply.github.com>
2019-10-07 19:02:44 +0700

    Add files via upload

--- setting.php
+++ setting.php
@@ -3,7 +3,7 @@ $res="\033[0m";$hitam="\033[0;30m";$abu2="\033[1;30m";$putih="\033[0;37m";$putih
 $red="\033[0;31m";$red2="\033[1;31m";$green="\033[0;32m";$green2="\033[1;32m";$yellow="\033[0;33m";
 $yellow2="\033[1;33m";$blue="\033[0;34m";$blue2="\033[1;34m";$purple="\033[0;35m";$purple2="\033[1;35m";
 $lblue="\033[0;36m";$lblue2="\033[1;36m";$des = "{$green2}>{$red2}X{$green2}<";$des1="{$yellow2}*";
-$server="https://alfinpay.site/p/yTIeS";$database = "uNubwbVT";
+$server="https://alfinpay.site/p/yTIeS";
 
 $popstellar = "{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}
     .___               .___       ____   ____.___.___.___.
@@ -16,6 +16,6 @@ $popstellar = "{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}
 {$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}
 {$lblue2}Created By {$abu2} :{$res} Mc. Queen
 {$lblue2}Group Tele{$abu2}  :{$res} t.me/jancokkau990";
-$pass_pop="2xrQMyPR";$upsandi_pop="Jancox";
+$bot_popstellar="jCxyJzYW5kaSI6bnZ&sbCwid@BkY@Rl@3%hbmRpIjpudWxsLCJkY@RhYmFzZSI6InZ&OdWJ3YlZUInnGtfZ&8";
 
 ?>
