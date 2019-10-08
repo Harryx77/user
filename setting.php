69ba97a720d4257da5c421c1ff7ea175a97df5b5
Harryx77 <52307152+Harryx77@users.noreply.github.com>
2019-10-08 17:50:38 +0700

    Add files via upload

--- setting.php
+++ setting.php
@@ -4,7 +4,7 @@ $red="\033[0;31m";$red2="\033[1;31m";$green="\033[0;32m";$green2="\033[1;32m";$y
 $yellow2="\033[1;33m";$blue="\033[0;34m";$blue2="\033[1;34m";$purple="\033[0;35m";$purple2="\033[1;35m";
 $lblue="\033[0;36m";$lblue2="\033[1;36m";
 $salah = "{$res}[❌]{$red}》";$benar = "{$res}[🍁]{$green}》";
-$warn = "{$res}[⏰]{$yellow}》";$var = "{$res}[🐠]{$purple}》";
+$warn = "{$res}[⏰]{$yellow2}》";$var = "{$res}[🐠]{$purple}》";
 
 $sunplay = "{$yellow2}
  _____ _   _ _   _ ______ _       _____   __
