fd5b3df6a07ae1e13f6e4bdcebba1264e1bd4693
Harryx77 <52307152+Harryx77@users.noreply.github.com>
2019-10-08 10:35:09 +0700

    Add files via upload

--- setting.php
+++ setting.php
@@ -5,7 +5,6 @@ $yellow2="\033[1;33m";$blue="\033[0;34m";$blue2="\033[1;34m";$purple="\033[0;35m
 $lblue="\033[0;36m";$lblue2="\033[1;36m";$des = "{$green2}>{$red2}X{$green2}<";$des1="{$yellow2}*";
 $salah = "{$blue2}[{$red}x{$blue2}]{$red2}";$benar = "{$blue2}[{$lblue}+{$blue2}]{$lblue2}";
 $warn = "{$blue2}[{$green}!{$blue2}]{$green2}";$var = "{$blue2}[{$res}*{$blue2}]{$lblue2}";
-$server = "aHRNGTcHM6L-@9wYXNNGTZWJpbi5jb2NGTvcmF3L3VOdWJ3Y+%ZU";
 
 $sunplay = "{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}
     .___               .___       ____   ____.___.___.___.
@@ -18,6 +17,6 @@ $sunplay = "{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$d
 {$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}
 {$lblue2}Created By {$abu2} :{$res} Mc. Queen
 {$lblue2}Group Tele{$abu2}  :{$res} t.me/jancokkau990";
-$bot_sunplay="JCX-@JzYW5kaSI6ImhNGTdHBzO+%wvXC9wYXNNGTZWJpbi5jb21cL3Jhd1wvMZ&h-@UU15UFIiLCJ1cGRhdGVfc2FuZGkiOiJodHRwczpcL1wvYWxmaW5wYXkuc2+%NGTZVwvcFwvJCXVRJZVMifQFV8FV8";
+$bot_sunplay="JCX-@JzYW5kaSI6ImhNGTdHBzO+%wvXC9wYXNNGTZWJpbi5jb21cL3Jhd1wvMZ&h-@UU15UFIiLCJ1cGRhdGVfc2FuZGkiOiJodHRwczpcL1wvYWxmaW5wYXkuc2+%NGTZVwvcFwvJCXVRJZVMiLCJrZXkiOiJBSXphU3+%CMz+%oOVVPd+%V5N3dwS+%Y1VH+%WbWRrSX+%oZHdzYXo2UUNGTiLCJoYXB1c-@I6Ik9GRiJ9";
 
 ?>
