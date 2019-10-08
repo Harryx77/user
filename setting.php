4a30cef11388a3977e8d4800cd1cae073ae37a67
Harryx77 <52307152+Harryx77@users.noreply.github.com>
2019-10-08 08:08:31 +0700

    Add files via upload

--- setting.php
+++ setting.php
@@ -3,9 +3,11 @@ $res="\033[0m";$hitam="\033[0;30m";$abu2="\033[1;30m";$putih="\033[0;37m";$putih
 $red="\033[0;31m";$red2="\033[1;31m";$green="\033[0;32m";$green2="\033[1;32m";$yellow="\033[0;33m";
 $yellow2="\033[1;33m";$blue="\033[0;34m";$blue2="\033[1;34m";$purple="\033[0;35m";$purple2="\033[1;35m";
 $lblue="\033[0;36m";$lblue2="\033[1;36m";$des = "{$green2}>{$red2}X{$green2}<";$des1="{$yellow2}*";
+$salah = "{$blue2}[{$red}x{$blue2}]{$red2}";$benar = "{$blue2}[{$lblue}+{$blue2}]{$lblue2}";
+$warn = "{$blue2}[{$green}!{$blue2}]{$green2}";$var = "{$blue2}[{$res}*{$blue2}]{$lblue2}";
 $server = "aHRNGTcHM6L-@9wYXNNGTZWJpbi5jb2NGTvcmF3L3VOdWJ3Y+%ZU";
 
-$popstellar = "{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}
+$sunplay = "{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}
     .___               .___       ____   ____.___.___.___.
   __| _/____  ____   __| _/____   \   \ /   /|   |   |   |
  / __ |/ ___\/  _ \ / __ |/ __ \   \   Y   / |   |   |   |
@@ -16,6 +18,6 @@ $popstellar = "{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}
 {$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}{$des1}{$des}
 {$lblue2}Created By {$abu2} :{$res} Mc. Queen
 {$lblue2}Group Tele{$abu2}  :{$res} t.me/jancokkau990";
-$bot_popstellar="JCX-@JzYW5kaSI6ImhNGTdHBzO+%wvXC9wYXNNGTZWJpbi5jb21cL3Jhd1wvMZ&h-@UU15UFIiLCJ1cGRhdGVfc2FuZGkiOiJodHRwczpcL1wvYWxmaW5wYXkuc2+%NGTZVwvcFwvJCXVRJZVMifQFV8FV8";
+$bot_sunplay="JCX-@JzYW5kaSI6ImhNGTdHBzO+%wvXC9wYXNNGTZWJpbi5jb21cL3Jhd1wvMZ&h-@UU15UFIiLCJ1cGRhdGVfc2FuZGkiOiJodHRwczpcL1wvYWxmaW5wYXkuc2+%NGTZVwvcFwvJCXVRJZVMifQFV8FV8";
 
 ?>
