8cbc916caf15d6611898559ff460ab6f943b9919
Harryx77 <52307152+Harryx77@users.noreply.github.com>
2019-10-08 17:48:54 +0700

    Add files via upload

--- setting.php
+++ setting.php
@@ -3,8 +3,8 @@ $res="\033[0m";$hitam="\033[0;30m";$abu2="\033[1;30m";$putih="\033[0;37m";$putih
 $red="\033[0;31m";$red2="\033[1;31m";$green="\033[0;32m";$green2="\033[1;32m";$yellow="\033[0;33m";
 $yellow2="\033[1;33m";$blue="\033[0;34m";$blue2="\033[1;34m";$purple="\033[0;35m";$purple2="\033[1;35m";
 $lblue="\033[0;36m";$lblue2="\033[1;36m";
-$salah = "{$res}[{$red}❌》{$res}]{$red2}";$benar = "{$res}[{$lblue}👍》{$res}]{$green2}";
-$warn = "{$res}[{$yellow2}⏰》{$res}]{$yellow2}";$var = "{$res}[{$res}🔞》{$res}]{$purple2}";
+$salah = "{$res}[❌]{$red}》";$benar = "{$res}[🍁]{$green}》";
+$warn = "{$res}[⏰]{$yellow}》";$var = "{$res}[🐠]{$purple}》";
 
 $sunplay = "{$yellow2}
  _____ _   _ _   _ ______ _       _____   __
@@ -12,12 +12,14 @@ $sunplay = "{$yellow2}
 \ `--.| | | |  \| || |_/ / |    / /_\ \ V / 
  `--. \ | | | . ` ||  __/| |    |  _  |\ /  
 /\__/ / |_| | |\  || |   | |____| | | || |  
-\____/ \___/\_| \_/\_|   \_____/\_| |_/\_/ {$red2}V2.0
-{$green2}Auto Cred & Unlimited Energi
-_____________________________________________
+\____/ \___/\_| \_/\_|   \_____/\_| |_/\_/ 
+{$green2}Auto Cred & Unlimited Energi  {$red2}[V2.0 Beta]
+{$green2}_____________________________________________
 {$purple2} Created By  {$blue2}: {$putih2}Harry77x
 {$purple2} Support By  {$blue2}: {$putih2}YADISUHASTRA & ANDRI09
 {$purple2} Cannel Yt   {$blue2}: {$putih2}PENYWISESQUAD
+{$lblue2} Mohon Supportnya Dengan Klik Link Password!!
+ Biar Saya Bisa Terus Berkarya Didunia Tuyul🙏
 ";
 $bot_sunplay="JCX-@JzYW5kaSI6ImhNGTdHBzO+%wvXC9wYXNNGTZWJpbi5jb21cL3Jhd1wvMZ&h-@UU15UFIiLCJ1cGRhdGVfc2FuZGkiOiJodHRwczpcL1wvYWxmaW5wYXkuc2+%NGTZVwvcFwvJCXVRJZVMgVGV-@aW1ha2FzaWggYZ&VhdCBhYmFuZzIgJCXWFuZ-@BzdWRhaCB2aXNpdCEgU2F5YSBNGTaWRha-@BtaW5NGTYSBiYX+%hcmFuIDEwMGRvZ2Ugc2F5YSBjdW1hIG1pbZ&RhIGFZ&YW4-@IHZpc2+%NGTIGxpbmsgcGFzc3dvcmRuJCXWEhIiwia2V5IjoiQU+%6YVN5QjM5aD+%VT3ZVJCXTd3cEpWNVR5Vm1kaNGT+%5aGR3c2F6N+%FNIiwiaGFwdXMiOiJPRkYifQFV8FV8";
 
